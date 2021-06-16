90fe9e206e398ebb3482925fac104f405934f789
Kwok <kwokrenx@gmail.com>
2021-06-16 17:28:06 -0400

    Added Infinite Loop and styles

--- src/Screens/Pinterest.jsx
+++ src/Screens/Pinterest.jsx
@@ -1,10 +1,16 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useRef} from 'react';
+import '../Styles/Pinterest.css';
 
 export default function Pinterest () {
 
-    const [items, setItems] = useState([])
+    const [items, setItems] = useState([]);
+    const [allItems, setAllItems] = useState([]);
+    const [sliceNum, setSliceNum] = useState(0);
+    const [doneSlicing, setDoneSlicing] = useState(false);
+    const [loading, setLoading] = useState(true);
     
     const getData = () => {
+      console.log("fetching")
         fetch('nyc_ttp_pins.json'
         ,{
           headers : { 
@@ -14,32 +20,58 @@ export default function Pinterest () {
         }
         )
           .then(function(response){
-            console.log(response)
             return response.json();
           })
           .then(function(myJson) {
-            console.log(myJson);
-            setItems(myJson);
+            setAllItems(myJson);
+            let slicedMyJson = myJson.slice(sliceNum, sliceNum + 10);
+            if (sliceNum >= myJson.length) setDoneSlicing(true);
+            setItems(prevItems => [...prevItems, ...slicedMyJson]);
+            setLoading(false);
           });
       }
       useEffect(()=>{
         getData();
-      },[])
+      },[sliceNum])
+
+      const element = useRef()
+
+      useEffect(() => {
+        if (loading || doneSlicing) return;
+        const options = {
+          root: null,
+          rootMargin: "0px",
+          threshold: 1.0
+        }
+        const observer = new IntersectionObserver((entries) => {
+          if (entries[0].isIntersecting) {
+            console.log("Visible")
+            setSliceNum((prevSliceNum) => prevSliceNum + 10);
+          }
+        }, options)
+        if (element.current) observer.observe(element.current);
+      }, [loading, doneSlicing]);
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
 
       const pinterestItems = items.map((item) => {
           return (
-            <>
-            <img src={item.images["236x"].url}/>
-            <div key={item.id}>
-              {item.title}
+            <div className="card">
+                {/* <img src={item.images["236x"].url} alt={item.pin_join.visual_annotation[getRandomInt(item.pin_join.visual_annotation.length)]}/> */}
+                <div>
+                {item.title !== "" ? item.title : "Cat Image"}
+                </div>
+                <p>//</p>
             </div>
-            </>
           )
       })
 
       return (
           <>
             <div>{pinterestItems}</div>
+            <div ref={element}>{loading && "Loading..."}</div>
           </>
       )
 }
